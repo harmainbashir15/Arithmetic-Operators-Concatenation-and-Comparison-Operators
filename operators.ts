826e62090c5e78e77c1e52bf2506fd4f6fdbb4a7
Harmain Bashir <162583900+harmainbashir15@users.noreply.github.com>
2024-03-09 07:20:34 -0800

    Add files via upload

---
+++ operators.ts
@@ -0,0 +1,259 @@
+//ARITHMETIC OPERATORS
+
+// ADDITION
+
+const num1 = 15
+const num2 = 75
+const sum = num1 + num2
+console.log(sum)
+
+const num3 = 44
+const num4 = 38
+const sum = num3 + num4
+console.log(sum)
+
+const studentsIn1class = 11
+const studentsIn3class = 22
+console.log(studentsIn1class + studentsIn3class)
+
+const peopleIn1stroom = 3
+const peopleIn2ndroom = 9
+console.log(peopleIn1stroom + peopleIn2ndroom)
+
+const peopleInredbus = 58
+const peopleIngreenbus = 33
+console.log(peopleInredbus + peopleIngreenbus )
+
+const coinsInjar1 = 12
+const coinsInjar2 = 19
+console.log(coinsInjar1 + coinsInjar2)
+
+const peopleInhouse1 = 7
+const peopleInhouse2 = 4
+console.log(peopleInhouse1 + peopleInhouse2)
+
+const breadsinplate1 = 4
+const breadsinplate2 = 6
+console.log(breadsinplate1 + breadsinplate2)
+
+const chairsontable1 = 6
+const chairsontable2 = 8
+console.log(chairsontable1 + chairsontable2 )
+
+const applesInbag1 = 5
+const applesInbag2 = 9
+console.log(applesInbag1 + applesInbag2)
+
+//SUBTRACTION
+
+const teachersInclass1 = 7
+const teachersInclass2 = 2
+console.log(teachersInclass1 - teachersInclass2)
+
+const scoreofmatch1 = 244
+const scoreofmatch2 = 169
+console.log(scoreofmatch1 - scoreofmatch2 )
+
+const toysatshop1 = 88
+const toysatshop2 = 50
+console.log(toysatshop1 - toysatshop2)
+
+const peopleInconcert1 = 500
+const peopleInconcert2 = 366
+console.log(peopleInconcert1 - peopleInconcert2)
+
+const accountbalanceofperson1 = 1000
+const accountbalanceofperson2 = 400
+console.log(accountbalanceofperson1 - accountbalanceofperson2)
+
+const bikesInshowroom1 =  77
+const bikesInshowroom2 =  56
+console.log(bikesInshowroom1 - bikesInshowroom2 )
+
+//MULTIPLICATION
+
+const mangosInplate1 = 10
+const mangosInplate2 = 7
+console.log(mangosInplate1 * mangosInplate2)
+
+const workersInbank1 = 229
+const workersInbank2 = 122
+console.log(workersInbank1  * workersInbank2)
+
+const teacupsIntry1 = 6
+const teacupsIntry2 = 4
+const mul = teacupsIntry1 * teacupsIntry2
+console.log(mul)
+
+const glassIntry1 =  13
+const glassIntry2 = 11
+ const mul = glassIntry1 * glassIntry2
+ console.log(mul)
+
+//DIVISION
+
+const lipsticksInbox1 = 23
+const lipsticksInbox2 = 19
+const div = lipsticksInbox1 / lipsticksInbox2
+console.log(div)
+
+const laptopsontable1 = 4
+const laptopsontable2 = 3
+const div =  laptopsontable1 / laptopsontable2
+console.log(div)
+
+const personsIncar1 = 5
+const personsIncar2 = 4
+const div = personsIncar1 / personsIncar2
+console.log(div)
+
+const studentsInbus1 = 34
+const studentsInbus2 = 22
+const div = studentsInbus1 / studentsInbus2
+console.log(div)
+
+const dressesInshop1 = 300
+const dressesInshop2 = 267
+const div = dressesInshop1 /dressesInshop2
+console.log(div)
+
+
+
+//MODULUS
+
+const pencils = 14
+const students = 7
+const mod = pencils % students
+console.log(mod)
+
+const apples = 20
+const childrens = 10
+const mod = apples % childrens
+console.log(mod)
+
+const sandwiches = 12
+const peoples = 7
+const mod = sandwiches % peoples
+console.log(mod)
+
+const  registers =  14
+const  teachers =  9
+const mod = registers % teachers
+console.log(mod)
+
+const guns = 33
+const officers = 15
+const mod = guns % officers
+console.log(mod)
+
+
+//POWER
+
+const england = 9 
+const nepal = 3
+const power = england ** nepal
+console.log(power);
+
+const pakistan = 5
+const india = 2
+const power = pakistan ** india
+console.log(power)
+
+const islamabadunited = 9
+const karachikings = 3
+const power = islamabadunited ** karachikings
+console.log(power) 
+
+const lahoreqalanders = 5
+const multansultan = 5
+const power = lahoreqalanders ** multansultan
+console.log(power)
+
+const num1 = 9
+const num2 = 3
+const power = num1 ** num2
+console.log(power)
+
+// CONCATENATION
+
+const firstName = "HARMAIN"
+const lastName = "FATIMA"
+console.log(firstName + " " + lastName)
+
+const mymothername = "RUQIA"
+const myfathername = "BASHIR"
+console.log( mymothername + " " + myfathername)
+
+const headcoach = "DAREN SAMMY"
+const bowlingcoach = "UMER GUL"
+console.log(headcoach + " " + bowlingcoach)
+
+const batsmen = "SHAHID AFRIDI"
+const bowler = "SAEED AJMAL"
+console.log(batsmen + " " + bowler)
+
+const team1 = "PAKISTAN"
+const team2 = "INDIA"
+console.log(team1 + " " + team2)
+
+
+
+
+let x = 10
+x = x + 6
+console.log(x)
+
+let a = 44
+a = a +65
+console.log(a)
+
+let h = 15
+h = h + 7
+console.log(h)
+
+let r = 95
+r = r + 7
+console.log(r)
+
+let f = 22
+f = f + 41
+console.log(f)
+
+
+
+//COMPARISON OPERATORS
+
+console.log(10 == 10)
+console.log(5>3)
+console.log(9<14)
+console.log(7 != 7)
+console.log(6 == 6)
+console.log(2 >= 5)
+console.log(5 >= 5)
+console.log(8 >= 10)
+console.log(3 <= 4)
+console.log(9 <= 15)
+console.log(4 > 3)
+console.log(6 < 11)
+console.log(0 != 0)
+
+
+
+//////                                                THE END                              \\\\\
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
